f5d17939719626a986f283e620125d14224c68f5
flan the dev <flanthedev@gmail.com>
2017-10-06 20:05:17 +0800

    complete problem set 2

--- ps1c.py
+++ ps1c.py
@@ -1,9 +1,8 @@
-# annual_salary = int(input("Enter the yearly salary: "))
-annual_salary = 150000
+annual_salary = int(input("Enter the yearly salary: "))
 monthly_salary = annual_salary/12
 semi_annual_raise = .07
 annual_return_rate = .04
-monthly_return_rate = (annual_return_rate/12)
+monthly_return_rate = annual_return_rate / 12
 number_of_months = 36
 down_payment = 250000
 monthly_savings = 0
@@ -12,21 +11,24 @@ high = 1.0
 
 total_earned = 0
 for month in range(1,37):
-  if month % 6 == 0:
-    monthly_salary += monthly_salary * semi_annual_raise
   this_months_return = total_earned * monthly_return_rate
   total_earned += monthly_salary + this_months_return
+  if month % 6 == 0:
+    monthly_salary += monthly_salary * semi_annual_raise
 total_earned = round(total_earned, 2)
-print('---------------------------------')
-print('total earned over ' + str(month) + ' months: ' + str(total_earned))
 
 solved = False
+count = 0
+
+if total_earned < down_payment:
+  solved = True
+
 while solved == False:
+  count += 1
+  savings_rate = (low+high) / 2
+  total_saved = total_earned * savings_rate
 
-  savings_rate = round(((low+high)/2), 4)
-  total_saved = round((total_earned * savings_rate), 2)
-
-  print("low: " + str(low) + "... rate: " + str(savings_rate) + "... high: " + str(high) + '... total saved: ' + str(total_saved))
+  # print("low: " + str(low) + "... rate: " + str(savings_rate) + "... high: " + str(high) + '... total saved: ' + str(total_saved))
   
   if total_saved > (down_payment-100) and total_saved < (down_payment+100):
     solved = True
@@ -35,5 +37,9 @@ while solved == False:
   elif total_saved < down_payment:
     low = savings_rate
 
-
-print("success! optimal savings rate:")
+if solved == True and total_earned < down_payment:
+  print("It is not possible to pay the down payment in three years.")
+elif solved == True:
+  print("success! optimal savings rate: " + str(round(savings_rate, 4)))
+  print("steps in bisection search: " + str(count))
+  
